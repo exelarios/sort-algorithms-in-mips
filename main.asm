2427adbecc657bd651367157f5194860e50780b6
Nigel Fang <nigel.fang00@gmail.com>
2021-05-11 20:10:34 -0700

    Added bubbleSort algo to main.asm

--- main.asm
+++ main.asm
@@ -12,7 +12,7 @@ prompt1: .asciiz "Which sorting algorithm do you want to run?\n"
 invalidInput: .asciiz "Invalid input, please try again."
 option1: .asciiz "1. Selection Sort\n"
 option2: .asciiz "2. Quick Sort\n"
-option3: .asciiz "3. Something Sort\n"
+option3: .asciiz "3. Bubble Sort\n"
 option4: .asciiz "4. Something Sort\n"
 testPrompt1: .asciiz "Hello"
 newLine: .asciiz "\r\n"
@@ -76,6 +76,7 @@ choice2:
 	j exit
 	
 choice3:
+	jal bubbleSort
 	j exit
 	
 choice4:
@@ -96,8 +97,61 @@ quickSort:
 	la $a0, testPrompt1
 	li $v0, 4
 	syscall
+bubbleSort:
+	la $a0, array		# a0 = array base address 
+	la $a1, array
+	li $t0, -1		# t0 is index, start at -1 bc addi at start of out_loop
+	li $t1, 0		# t1 is index2	
+	li $t2, 15		# t2 is length of array
+	li $s0, 14		# s0 = array length - 1
+	li $s1, 14		# s1 = array length - 1 - index
 	
+	out_loop:
+		addi $t0, $t0, 1		# index++
+		bge $t0, $t2, finish_sort	# if index >= array length - 1, exit loop	
+		j in_loop			# go to inner loop
+	
+	in_loop:
+		li $t1, 0			# index2 = 0
+		la $a0, array
+		sub $s1, $s0, $t0		# calc array length - index - 1 once per outer loop run
+		j in_loop_content		# go to inner loops content
+		
+	in_loop_content:
+		bge $t1, $s1, out_loop		# if index2 >= array length - 1 - index, exit to outer loop
+		lw $t4, 0($a0)			# t4 = array[index2]
+		lw $t5, 4($a0)			# t5 = array[index2 + 1]
+		bgt $t4, $t5, swap		# if ( array[index2] > array[index2 + 1], then swap
+		addi $a0, $a0, 4		# increment base address
+		addi $t1, $t1, 1		# index2++
+		bge $t1, $s1, out_loop		# check if the increment made t1 > s1
+		blt $t1, $s1, in_loop_content	# if index2 < array length - index - 1, go back to top of inner loop
+	swap:
+		sw $t4, 4($a0)			# array[index2 + 1] = array[index2]
+		sw $t5, 0($a0)			# array[index2] = array[index2 + 1]
+		addi $a0, $a0, 4		# increment base address
+		addi $t1, $t1, 1		# index2++
+		j in_loop_content
+		
+	finish_sort:
+		jr $ra
+		
+exit_loop:
+	bge $t6, 15, exit		# if we reach end of array (15 words), exit program
+	lw $t2, 0($a1)			# t2 = array[current]
+	addi $a1, $a1, 4		# increment base address to next word
+		
+	li $v0, 1			# 1 is syscall code for print int
+	move $a0, $t2			# t2 is to be printed
+	syscall				# print t2
+		
+	li $a0, 32			# 32 is the space character so array is readable
+	li $v0, 11			# 11 is syscall code to print character
+	syscall				# print space character
+	
+	addi $t6, $t6, 1		# increment counter 
+	j exit_loop			# go back to beginning of loop
+		
 exit:
-	li $v0, 10
-	syscall
-	
+	li $v0, 10			# 10 is syscall code for exit
+	syscall				# exit
