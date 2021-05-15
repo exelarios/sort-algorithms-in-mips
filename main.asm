1cebd425621030700aa51a97c3e9fef7a8d46f0a
Saharat12 <80376456+Saharat12@users.noreply.github.com>
2021-05-15 01:06:38 -0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -116,7 +116,7 @@ choice1:
 	j exit
 
 choice2:
-	jal quickSort
+	jal quickSortStart
 	j exit
 	
 choice3:
@@ -196,10 +196,181 @@ selectionSort:
 	addi $sp, $sp, 44
 	jr $ra
 
+quickSortStart:
+
+	move		$a0, $s1
+	addi		$s0, $s0, -1
+	move		$a2, $s0
+	
+	jal 		quickSort
+	
+	jal		PRINT
+	
+	j		exit2
+	
+PRINT:
+	move		$t0, $s0
+	addi		$t0, $t0, 1
+	move		$s0, $s1
+
+	
+	Loop:
+		beq		$t0, $zero, out_print
+		
+
+	# printing Array elements
+		li		$v0, 1
+		lw		$a0, 0($s0)
+		syscall
+		
+		li		$v0, 4
+		la		$a0, space
+		syscall
+	
+		addi		$t0, $t0, -1
+		addi		$s0, $s0, 4
+	
+		j		Loop
+	out_print:
+		# make new line
+		li		$v0, 4
+		la		$a0, newLine
+		syscall
+		jr		$ra
+
 quickSort:
-	la $a0, testPrompt1
-	li $v0, 4
-	syscall
+	addi		$sp, $sp, -24	
+	sw		$s0, 0($sp)	
+	sw		$s1, 4($sp)	
+	sw		$s2, 8($sp)	
+	sw		$a1, 12($sp)	
+	sw		$a2, 16($sp)	
+	sw		$ra, 20($sp)	
+
+	move	$s0, $a1		# l = left
+	move	$s1, $a2		# r = right
+	move	$s2, $a1		# p = left
+
+# while (l < r)
+while:
+	bge		$s0, $s1, done
+	
+# while (arr[l] <= arr[p] && l < right)
+Loop1:
+	li		$t7, 4			# t7 = 4
+	# t0 = &arr[l]
+	mult		$s0, $t7
+	mflo		$t0			# t0 =  l * 4bit
+	add		$t0, $t0, $a0		# t0 = &arr[l]
+	lw		$t0, 0($t0)
+	# t1 = &arr[p]
+	mult		$s2, $t7
+	mflo		$t1			# t1 =  p * 4bit
+	add		$t1, $t1, $a0		# t1 = &arr[p]
+	lw		$t1, 0($t1)
+	# check arr[l] <= arr[p]
+	bgt		$t0, $t1, Loop1_done
+	# check l < right
+	bge		$s0, $a2, Loop1_done
+	# l++
+	addi		$s0, $s0, 1
+	j		Loop1
+	
+Loop1_done:
+
+# while (arr[r] >= arr[p] && r > left)
+Loop2:
+	li		$t7, 4			# t7 = 4
+	# t0 = &arr[r]
+	mult		$s1, $t7
+	mflo		$t0			# t0 =  r * 4bit
+	add		$t0, $t0, $a0		# t0 = &arr[r]
+	lw		$t0, 0($t0)
+	# t1 = &arr[p]
+	mult		$s2, $t7
+	mflo		$t1			# t1 =  p * 4bit
+	add		$t1, $t1, $a0		# t1 = &arr[p]
+	lw		$t1, 0($t1)
+	
+	blt		$t0, $t1, Loop2_done	# check arr[r] >= arr[p]
+	ble		$s1, $a1, Loop2_done	# check r > left
+
+	addi		$s1, $s1, -1			# r--
+	j		Loop2
+	
+Loop2_done:
+
+# if (l >= r)
+	blt		$s0, $s1, If
+# SWAP (arr[p], arr[r])
+	li		$t7, 4			# t7 = 4
+	# t0 = &arr[p]
+	mult		$s2, $t7
+	mflo		$t6			# t6 =  p * 4bit
+	add		$t0, $t6, $a0		# t0 = &arr[p]
+	# t1 = &arr[r]
+	mult		$s1, $t7
+	mflo		$t6			# t6 =  r * 4bit
+	add		$t1, $t6, $a0		# t1 = &arr[r]
+	# Swap
+	lw		$t2, 0($t0)
+	lw		$t3, 0($t1)
+	sw		$t3, 0($t0)
+	sw		$t2, 0($t1)
+	
+# quick(arr, left, r - 1)
+	# set arguments
+	move		$a2, $s1
+	addi		$a2, $a2, -1		# a2 = r - 1
+	jal		quickSort
+	# pop stack
+	lw		$a1, 12($sp)		# load a1
+	lw		$a2, 16($sp)		# load a2
+	lw		$ra, 20($sp)		# load ra
+	
+# quick(arr, r + 1, right)
+	# set arguments
+	move		$a1, $s1
+	addi		$a1, $a1, 1		# a1 = r + 1
+	jal		quickSort
+	# pop stack
+	lw		$a1, 12($sp)		# load a1
+	lw		$a2, 16($sp)		# load a2
+	lw		$ra, 20($sp)		# load ra
+	
+# return
+	lw		$s0, 0($sp)		# load s0
+	lw		$s1, 4($sp)		# load s1
+	lw		$s2, 8($sp)		# load s2
+	addi		$sp, $sp, 24		# Adjest sp
+	jr		$ra
+
+If:
+
+# SWAP (arr[l], arr[r])
+	li		$t7, 4			# t7 = 4
+# t0 = &arr[l]
+	mult		$s0, $t7
+	mflo		$t6			# t6 =  l * 4bit
+	add		$t0, $t6, $a0		# t0 = &arr[l]
+# t1 = &arr[r]
+	mult		$s1, $t7
+	mflo		$t6			# t6 =  r * 4bit
+	add		$t1, $t6, $a0		# t1 = &arr[r]
+# Swap
+	lw		$t2, 0($t0)
+	lw		$t3, 0($t1)
+	sw		$t3, 0($t0)
+	sw		$t2, 0($t1)
+	
+	j		while
+	
+done:
+	lw	$s0, 0($sp)	
+	lw	$s1, 4($sp)		
+	lw	$s2, 8($sp)		
+	addi	$sp, $sp, 24	
+	jr	$ra
 	
 bubbleSort:
 	move $a0, $s1		# a0 = array base address 
@@ -290,5 +461,8 @@ print:
 	
 exit:
 	jal print
+	li $v0, 10
+	syscall
+exit2:
 	li $v0, 10
 	syscall
